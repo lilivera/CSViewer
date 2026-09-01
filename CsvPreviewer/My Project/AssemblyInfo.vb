555f41e23939837481c6314afa8c36cafdddef31
lilivera <38494952+lilivera@users.noreply.github.com>
2026-09-01 11:42:51 +0900

    Release v1.0.1

--- CsvPreviewer/My Project/AssemblyInfo.vb
+++ CsvPreviewer/My Project/AssemblyInfo.vb
@@ -8,5 +8,5 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © 2026")>
 <Assembly: ComVisible(False)>
 <Assembly: Guid("9fcb3fd6-312d-4ab5-88f6-8bd6d00ea59e")>
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.1.0")>
+<Assembly: AssemblyFileVersion("1.0.1.0")>
